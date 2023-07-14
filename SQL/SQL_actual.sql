20220a78ec2f38071300d80bb4810ecc35883b74
M7CKZ <71125258+M7CKZ@users.noreply.github.com>
2023-07-14 12:25:01 +0400

    Update SQL_actual.sql

--- SQL/SQL_actual.sql
+++ SQL/SQL_actual.sql
@@ -43,8 +43,8 @@ BEGIN
  --------------------------------------------------------------------------------------------
 
  declare
-  @autoZakazOrderTitleId int = JSON_VALUE(@json, '$.auto_zakaz_order_title_id')	 -- идентификатор заказа
- ,@order				 int = JSON_VALUE(@json, '$.order')						 -- кол-во штук к заказу
+  @autoZakazOrderTitleId 	int = JSON_VALUE(@json, '$.auto_zakaz_order_title_id')	 	-- идентификатор заказа
+ ,@order			int = JSON_VALUE(@json, '$.order')				-- кол-во штук к заказу
 
  --------------------------------------------------------------------------------------------
 
@@ -54,35 +54,35 @@ BEGIN
 
  -- список необходимых данных для расчета
  declare @tblDataList table (
-   autoZakazDataId	int			  -- идетнтификатор товара из расчета потребности
-  ,[data]			nvarchar(max) -- предложения поставщиков, рекомендованные поставщики
+   autoZakazDataId	int		-- идетнтификатор товара из расчета потребности
+  ,[data]		nvarchar(max) 	-- предложения поставщиков, рекомендованные поставщики
  )
 
  -- список товаров с их заказанными идентификаторами
  declare @tblOrderIdsList table (
    autoZakazOrderDataId int -- идентификатор товара в размещенном заказе
-  ,autoZakazDataId		int -- идетнтификатор товара из расчета потребности
+  ,autoZakazDataId	int -- идетнтификатор товара из расчета потребности
  )
 
  -- список предложений к заказу
  declare @tblOffersList table (
-   auto_zakaz_data_id int			  -- идетнтификатор товара из расчета потребности
-  ,reg_id			  int			  -- идентификатор товар с учетом производителя
-  ,drug_id			  int			  -- идентификатор наименования товара
-  ,form_id			  int			  -- идентификатор формы выпуска
-  ,fabr_id			  int			  -- идентификатор производителя
-  ,distr_id_web		  int			  -- идентификатор поставщика веб
-  ,distr_price_id	  bigint		  -- идентификатор прайс-листа
-  ,distr_price		  numeric (15, 2) -- цена
-  ,distr_ost		  int			  -- остатки
-  ,distr_min_zakaz    smallint		  -- минимальный заказ
-  ,distr_ratio		  smallint		  -- кратность
-  ,distr_name		  varchar(150)	  -- наименования поставщика
-  ,distr_price_fabr   numeric (15, 2) -- цена производителя
-  ,k_eff			  numeric (15, 5) -- коэффициент эффективности
-  ,drug_name		  varchar(255)	  -- наименование товара
-  ,form_name		  varchar(255)	  -- наименование формы
-  ,fabr_name		  varchar(255)	  -- наименование производителя
+   auto_zakaz_data_id 	int 		-- идетнтификатор товара из расчета потребности
+  ,reg_id		int 		-- идентификатор товар с учетом производителя
+  ,drug_id		int 		-- идентификатор наименования товара
+  ,form_id		int 		-- идентификатор формы выпуска
+  ,fabr_id		int 		-- идентификатор производителя
+  ,distr_id_web		int 		-- идентификатор поставщика веб
+  ,distr_price_id	bigint 		-- идентификатор прайс-листа
+  ,distr_price		numeric (15, 2) -- цена
+  ,distr_ost		int		-- остатки
+  ,distr_min_zakaz    	smallint	-- минимальный заказ
+  ,distr_ratio		smallint	-- кратность
+  ,distr_name		varchar(150)	-- наименования поставщика
+  ,distr_price_fabr   	numeric (15, 2) -- цена производителя
+  ,k_eff		numeric (15, 5)	-- коэффициент эффективности
+  ,drug_name		varchar(255)	-- наименование товара
+  ,form_name		varchar(255)	-- наименование формы
+  ,fabr_name		varchar(255)	-- наименование производителя
  )
 
  -- список рекомендованных маркетинговых поставщиков
@@ -95,8 +95,8 @@ BEGIN
 
  -- заказанный товар
  declare @tblOrder table (
-	auto_zakaz_data_id int	  -- идетнтификатор товара из расчета потребности
-   ,price_id		   bigint -- идентификатор прайс-листа
+    auto_zakaz_data_id	int	-- идетнтификатор товара из расчета потребности
+   ,price_id		bigint 	-- идентификатор прайс-листа
  )
 
  -- список комментариев при редактировании заказа
@@ -122,13 +122,14 @@ BEGIN
 
  -- собираем необходимые данные для расчета
  insert into @tblDataList(
-	autoZakazDataId
+    autoZakazDataId
    ,[data]
  )
- select az.AutoZakazDataId
-	   ,az.OffersAndSuppliers
+ select  az.AutoZakazDataId
+	,az.OffersAndSuppliers
  from Miracle.dbo.AutoZakazExtraData az with (index = Ind1)
- join @tblProducts t on t.[value] = az.AutoZakazDataId
+ join @tblProducts t 
+ on t.[value] = az.AutoZakazDataId
 
  --------------------------------------------------------------------------------------------
 
@@ -159,22 +160,22 @@ BEGIN
   from openjson([data], '$.offers')
   with (
     auto_zakaz_data_id int
-   ,reg_id			 int
-   ,drug_id			 int
-   ,form_id			 int
-   ,fabr_id			 int
-   ,distr_id_web	 int
-   ,distr_price_id	 bigint
-   ,distr_price		 numeric (15, 2)
-   ,distr_ost		 int
-   ,distr_min_zakaz  smallint
-   ,distr_ratio		 smallint
-   ,distr_name		 varchar(150)
-   ,distr_price_fabr numeric (15, 2)
-   ,k_eff			 numeric (15, 5)
-   ,drug_name		 varchar(255)
-   ,form_name		 varchar(255)
-   ,fabr_name		 varchar(255)
+   ,reg_id		int
+   ,drug_id		int
+   ,form_id		int
+   ,fabr_id		int
+   ,distr_id_web	int
+   ,distr_price_id	bigint
+   ,distr_price		numeric (15, 2)
+   ,distr_ost		int
+   ,distr_min_zakaz  	smallint
+   ,distr_ratio		smallint
+   ,distr_name		varchar(150)
+   ,distr_price_fabr 	numeric (15, 2)
+   ,k_eff 		numeric (15, 5)
+   ,drug_name		varchar(255)
+   ,form_name		varchar(255)
+   ,fabr_name		varchar(255)
   )
  ) oa
 
@@ -182,7 +183,7 @@ BEGIN
 
  -- заполнение рекомендованных поставщиков
  insert into @tblRecommendedSuppliers(
-	reg_id
+    reg_id
    ,drug_id
    ,form_id
    ,distr_id
@@ -193,7 +194,7 @@ BEGIN
   select *
   from openjson([data], '$.recommended_suppliers')
   with (
-    reg_id	 int
+    reg_id   int
    ,drug_id  int
    ,form_id  int
    ,distr_id int
@@ -211,17 +212,15 @@ BEGIN
 	   ,price_id
 	 )
 	 select top 1 t.auto_zakaz_data_id
-				 ,t.distr_price_id
+		     ,t.distr_price_id
 	 from @tblOffersList t
-	 join @tblRecommendedSuppliers tr on tr.drug_id = t.drug_id
-									 and tr.form_id = t.form_id
-									 and tr.distr_id = t.distr_id_web
-	 where -- остаток
-		   t.distr_ost >= @order
-		   -- минимальный заказ
-	   and @order >= t.distr_min_zakaz
-		   -- кратность
-	   and ((@order % t.distr_ratio) = 0)
+	 join @tblRecommendedSuppliers tr 
+	 on tr.drug_id = t.drug_id and 
+	    tr.form_id = t.form_id and 
+	    tr.distr_id = t.distr_id_web
+	 where t.distr_ost >= @order and 
+	       @order >= t.distr_min_zakaz and 
+	       ((@order % t.distr_ratio) = 0)
  end
 
  -- если не получилось на рекомендованного
@@ -231,15 +230,12 @@ BEGIN
 	    auto_zakaz_data_id
 	   ,price_id
 	 )
-	 select top 1 t.auto_zakaz_data_id
-				 ,t.distr_price_id
+	select top 1 t.auto_zakaz_data_id
+		    ,t.distr_price_id
 	from @tblOffersList t
-	where -- остаток
-		  t.distr_ost >= @order
-		  -- минимальный заказ
-	  and @order >= t.distr_min_zakaz
-		  -- кратность
-	  and ((@order % t.distr_ratio) = 0)
+	where t.distr_ost >= @order and
+	      @order >= t.distr_min_zakaz and 
+	      ((@order % t.distr_ratio) = 0)
  end
 
 ----------------------------------------------------------------------------------------------
@@ -254,36 +250,36 @@ BEGIN
  if not exists (select 1 from @tblOffersList where distr_ost >= @order)
   begin
     insert into @commentsWhenEditingOrder(
-		type_comments
-	   ,limitation
+		 type_comments
+	   	,limitation
 	)
-    select 1				as type_comments
-		  ,max(t.distr_ost) as limitation
-	from @tblOffersList t
+    select 1 			as type_comments
+	  ,max(t.distr_ost) 	as limitation
+    from @tblOffersList t
   end
 
  -- Проверка на минимальный заказ
  if not exists (select 1 from @tblOffersList where @order >= distr_min_zakaz)
   begin
     insert into @commentsWhenEditingOrder(
-		type_comments
+	    type_comments
 	   ,limitation
 	)
-    select 2					  as type_comments
-		  ,min(t.distr_min_zakaz) as limitation
-	from @tblOffersList t
+    select 2			  as type_comments
+	  ,min(t.distr_min_zakaz) as limitation
+    from @tblOffersList t
   end
 
  -- Проверка на кратность
  if not exists (select 1 from @tblOffersList where (@order % distr_ratio) = 0)
   begin
     insert into @commentsWhenEditingOrder(
-		type_comments
+	    type_comments
 	   ,limitation
 	)
-    select 3				as type_comments
-		  ,min(distr_ratio) as limitation
-	from @tblOffersList t
+    select 3			as type_comments
+	  ,min(distr_ratio) 	as limitation
+    from @tblOffersList t
   end
 end
 
@@ -294,8 +290,8 @@ end
 		 type_comments
 		,limitation
 	 )
-     select	 4 as type_comments
-			,0 as limitation
+     select 4 as type_comments
+	   ,0 as limitation
    end
 
  --------------------------------------------------------------------------------------------
@@ -308,11 +304,12 @@ end
 	 update azd
 	 set commentIds = null
 	 from Miracle.dbo.AutoZakazData azd
-	 join @tblProducts tp on tp.[value] = azd.autoZakazDataId
+	 join @tblProducts tp 
+	 on tp.[value] = azd.autoZakazDataId
 
 	 -- добавляем в список заказанных позиций
 	 insert Miracle.dbo.AutoZakazOrderData (
-		autoZakazOrderTitleId
+	    autoZakazOrderTitleId
 	   ,autoZakazDataId
 	   ,zakazToOrder
 	   ,price
@@ -324,31 +321,32 @@ end
 	   ,parentAutoZakazDataId
 	 )
 	 --------------------------------------------
-	 output inserted.autoZakazOrderDataId
-		   ,inserted.autoZakazDataId
+	 output  inserted.autoZakazOrderDataId
+		,inserted.autoZakazDataId
 	 into @tblOrderIdsList (
 	   autoZakazOrderDataId
 	  ,autoZakazDataId
 	 )
 	 --------------------------------------------
-	 select @autoZakazOrderTitleId as autoZakazOrderTitleId
-		   ,tp.auto_zakaz_data_id  as autoZakazDataId
-		   ,@order				   as zakazToOrder
-		   ,tp.distr_price		   as price
-		   ,tp.distr_id_web		   as distrId
-		   ,tp.reg_id			   as regId
-		   ,tp.distr_price_id	   as priceId
-		   ,tp.distr_price_fabr	   as priceFabr
-		   ,'1'					   as canBeExcluded
-		   ,'-1'				   as parentAutoZakazDataId
+	 select @autoZakazOrderTitleId 	as autoZakazOrderTitleId
+	       ,tp.auto_zakaz_data_id  	as autoZakazDataId
+	       ,@order			as zakazToOrder
+	       ,tp.distr_price		as price
+	       ,tp.distr_id_web		as distrId
+	       ,tp.reg_id		as regId
+	       ,tp.distr_price_id	as priceId
+	       ,tp.distr_price_fabr	as priceFabr
+	       ,'1'			as canBeExcluded
+	       ,'-1'			as parentAutoZakazDataId
 	 from @tblOrder t
-	 join @tblOffersList tp on t.price_id = tp.distr_price_id
+	 join @tblOffersList tp 
+	 on t.price_id = tp.distr_price_id
 	 where not exists (
 	  select 1
 	  from Miracle.dbo.AutoZakazOrderData azod
-	  where azod.autoZakazOrderTitleId = @autoZakazOrderTitleId
-		and azod.autoZakazDataId = tp.auto_zakaz_data_id
-		and azod.donorBranchId is null
+	  where azod.autoZakazOrderTitleId = @autoZakazOrderTitleId and 
+	        azod.autoZakazDataId = tp.auto_zakaz_data_id and 
+	        azod.donorBranchId is null
 	 )
 
  end
@@ -359,33 +357,36 @@ end
  begin
 
 	 -- возвращаем результат
-	 select o.autoZakazOrderDataId		 as auto_zakaz_order_data_id_order -- идентификатор товара в заказе
-		   ,tl.auto_zakaz_data_id		 as auto_zakaz_data_id			   -- идентификатор потребности товара
-		   ,tl.reg_id					 as reg_id						   -- идентификатор товара с учетом производителя
-		   ,tl.drug_id					 as drug_id						   -- идентификатор наименования товара
-		   ,tl.form_id					 as form_id						   -- идентификатор формы выпуска
-		   ,tl.distr_id_web				 as distr_id					   -- идентификатор поставщика
-		   ,tl.distr_price_id			 as price_id					   -- идентификатор прайс-листа
-		   ,tl.drug_name				 as drug						   -- наимнование товара
-		   ,tl.form_name				 as form						   -- наименование формы
-		   ,tl.fabr_name				 as fabr						   -- наименование производителя
-		   ,tl.distr_price				 as price						   -- цена предложения
-		   ,tl.distr_price				 as offer_price					   -- цена предложения
-		   ,tl.distr_price_fabr			 as price_fabr					   -- цена производителя
-		   ,@order						 as [order]						   -- заказ
-		   ,@order * tl.distr_price		 as sum_order					   -- сумма заказа по товару
-		   ,null						 as comment_ids					   -- комментарии - сбрасываем
-		   ,'-1'						 as parent_auto_zakaz_data_id	   -- в этом случае считается как коммент - отредактирован пользователем
-		   ,tl.k_eff					 as k_eff						   -- коэффициент эффективности
-		   ,'1'							 as isCountKeff					   -- признак того, что нужно считать эффективность
-		   ,'1'							 as can_be_excluded				   -- признак того, что товар может быть исключен из заказа по ограничению
-		   ,tl.distr_ratio				 as distr_ratio					   -- кратность заказанного поставщика
-		   ,tl.distr_min_zakaz			 as distr_min_zakaz				   -- минимальный заказ по поставщику
-		   ,tl.distr_ost				 as distr_ost					   -- остаток у поставщика
-		   ,tl.distr_name				 as distr						   -- наименования поставщика
+	 select o.autoZakazOrderDataId		as auto_zakaz_order_data_id_order 	-- идентификатор товара в заказе
+		   ,tl.auto_zakaz_data_id	as auto_zakaz_data_id			-- идентификатор потребности товара
+		   ,tl.reg_id			as reg_id				-- идентификатор товара с учетом производителя
+		   ,tl.drug_id			as drug_id				-- идентификатор наименования товара
+		   ,tl.form_id			as form_id				-- идентификатор формы выпуска
+		   ,tl.distr_id_web		as distr_id				-- идентификатор поставщика
+		   ,tl.distr_price_id		as price_id				-- идентификатор прайс-листа
+		   ,tl.drug_name		as drug					-- наимнование товара
+		   ,tl.form_name		as form					-- наименование формы
+		   ,tl.fabr_name		as fabr					-- наименование производителя
+		   ,tl.distr_price		as price				-- цена предложения
+		   ,tl.distr_price		as offer_price				-- цена предложения
+		   ,tl.distr_price_fabr		as price_fabr				-- цена производителя
+		   ,@order			as [order]				-- заказ
+		   ,@order * tl.distr_price	as sum_order				-- сумма заказа по товару
+		   ,null			as comment_ids				-- комментарии - сбрасываем
+		   ,'-1'			as parent_auto_zakaz_data_id	   	-- в этом случае считается как коммент - отредактирован пользователем
+		   ,tl.k_eff			as k_eff				-- коэффициент эффективности
+		   ,'1'				as isCountKeff				-- признак того, что нужно считать эффективность
+		   ,'1'				as can_be_excluded			-- признак того, что товар может быть исключен из заказа по ограничению
+		   ,tl.distr_ratio		as distr_ratio				-- кратность заказанного поставщика
+		   ,tl.distr_min_zakaz		as distr_min_zakaz			-- минимальный заказ по поставщику
+		   ,tl.distr_ost		as distr_ost				-- остаток у поставщика
+		   ,tl.distr_name		as distr				-- наименования поставщика
 	 from @tblOrder t
-	 join @tblOffersList tl on t.price_id = tl.distr_price_id and t.auto_zakaz_data_id = tl.auto_zakaz_data_id
-	 join @tblOrderIdsList o on tl.auto_zakaz_data_id = o.autoZakazDataId
+	 join @tblOffersList tl 
+	 on t.price_id = tl.distr_price_id and 
+	    t.auto_zakaz_data_id = tl.auto_zakaz_data_id
+	 join @tblOrderIdsList o
+	 on tl.auto_zakaz_data_id = o.autoZakazDataId
 
  end
  else
@@ -394,8 +395,8 @@ end
   -- иначе если заказ не разместился возвращаем комментарии почему
   select 0 as success
 
-  select type_comments -- тип коммента
-		,limitation	   -- установленный лимит ограничения (кратность, минимальный заказ, остаток)
+  select type_comments 	-- тип коммента
+	,limitation	-- установленный лимит ограничения (кратность, минимальный заказ, остаток)
   from @commentsWhenEditingOrder
 
  end
